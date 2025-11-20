7032882125608ee66fc1ac84d098ec559eac91de
deelaurence <laurie143pelumi@gmail.com>
2025-11-20 17:32:18 +0100

    port fix

--- src/main.ts
+++ src/main.ts
@@ -113,8 +113,8 @@ async function bootstrap() {
     },
   });
 
-  const port = process.env.PORT;
-  await app.listen(port);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 
   const protocol = useHttps ? 'https' : 'http';
   logger.log(`🚀 Admin API is running on: ${protocol}://localhost:${port}`);
